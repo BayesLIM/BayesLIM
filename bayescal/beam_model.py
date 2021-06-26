345906e613d3a4376ad0356d65394e79ae1cb56d
Nicholas Kern <nkern@berkeley.edu>
2021-06-25 21:24:00 -0400

    modified:   bayescal/beam_model.py

--- bayescal/beam_model.py
+++ bayescal/beam_model.py
@@ -500,7 +500,10 @@ class YlmResponse(PixelResponse):
         Ylm = self.get_Ylm(zen, az)
 
         # next do slower dot product over Ncoeff
-        beam = p @ Ylm.transpose(-1, -2)
+        if not torch.is_complex(p):
+            beam = p @ Ylm.transpose(-1, -2).real + 1j * (p @ Ylm.transpose(-1, -2).imag)
+        else:
+            beam = p @ Ylm.transpose(-1, -2)
 
         if self.powerbeam:
             if torch.is_complex(beam):
