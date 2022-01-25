6df19cca75f29186c9016fdd7a27382fae411b8e
Nicholas Kern <nkern@berkeley.edu>
2022-01-25 13:10:13 -0500

    added tri-linear interpolation to cube2map

--- bayeslim/cosmology.py
+++ bayeslim/cosmology.py
@@ -206,8 +206,39 @@ class Cosmology(FlatLambdaCDM):
         return 2 * np.pi / self.dRpara_df(z)
 
 
+def gauss1d(x, scale=1.0, loc=0.0):
+    """
+    Generate a 1D Gaussian window function
+    at provided points given window parameters
+
+    Parameters
+    ----------
+    x : ndarray
+        Locations to evaluate window. If 2D array
+        of shape (Nfeatures, Nsamples), window
+        normalization is along Nsamples dimension.
+    scale : float, optional
+        Standard deviation of Gaussian
+    loc : float, optional
+        Mean of Gaussian
+
+    Returns
+    -------
+    win : ndarray
+        Window values at provided radial points,
+        normalized such that sum(win) = 1
+    """
+    w = np.atleast_2d(np.exp(-0.5 * (x - loc)**2 / scale**2))
+    w /= w.sum(axis=1, keepdims=True)
+    if w.size == 1:
+        w = w[0, 0]
+
+    return w
+
+
 def cube2lcone(sims, sim_zs, freqs, sim_res, zinterp='nearest',
-               cosmo=None, nside=None, hpx=True, roll=None):
+               interp='nearest', cosmo=None, nside=None, hpx=True,
+               roll=None):
     """
     Project simulation cube onto a lightcone.
 
@@ -231,6 +262,9 @@ def cube2lcone(sims, sim_zs, freqs, sim_res, zinterp='nearest',
     zinterp : str, optional
         Line-of-sight (redshift) interpolation method
         ['nearest', 'linear', 'quadratic']
+    interp : str, optional
+        Spatial cube interpolation method ['nearest', 'linear'].
+        See cube2map() for details.
     cosmo : Cosmology object, optional
     nside: integer, optional
         NSIDE of the output HEALPix image.
@@ -304,13 +338,15 @@ def cube2lcone(sims, sim_zs, freqs, sim_res, zinterp='nearest',
             cube = a * zs[i]**2 + b * zs[i] + c
 
         # tile and sample onto a map
-        m = cube2map(cube, dcs[i], sim_res, nside=nside, hpx=hpx, roll=roll)
+        m = cube2map(cube, dcs[i], sim_res, nside=nside, hpx=hpx,
+                     roll=roll, interp=interp)
         lcone.append(m)
 
     return np.array(lcone), dcs
 
 
-def cube2map(cube, dc, sim_res, nside=None, hpx=True, roll=None):
+def cube2map(cube, dc, sim_res, nside=None, hpx=True, roll=None,
+             interp='nearest'):
     """
     Tile a simulation cube and extract
     a map at a fixed comoving distance away
@@ -334,6 +370,8 @@ def cube2map(cube, dc, sim_res, nside=None, hpx=True, roll=None):
         Before sampling the cube, roll along x, y, and/or z
         axis. If int, all x,y,z are rolled the same, or if
         tuple roll (x, y, z) separately.
+    interp : str, optional
+        Spatial cube method ['nearest', 'linear']
 
     Returns
     -------
@@ -347,19 +385,62 @@ def cube2map(cube, dc, sim_res, nside=None, hpx=True, roll=None):
         cube = np.roll(cube, roll, axis=(0, 1, 2))
 
     if hpx:
+        # tile onto a full sky healpix map
         assert utils.import_healpy
         # Get the vector coordinates (vx, vy, vz) of the HEALPIX pixels.
         vx, vy, vz = utils.healpy.pix2vec(nside, np.arange(utils.healpy.nside2npix(nside)))
 
-        # Translate vector coordinates to comoving coordinates and determine the
-        # corresponding cube indexes (xi, yi). Nearest neighbor interpolation
-        xi = np.mod(np.around(vx * dc / sim_res).astype(int), sim_size[0])
-        yi = np.mod(np.around(vy * dc / sim_res).astype(int), sim_size[1])
-        zi = np.mod(np.around(vz * dc / sim_res).astype(int), sim_size[2])
-        out = np.asarray(cube[xi, yi, zi])
+        # translate hpx vector coords to comoving coords
+        # and put into cube index units
+        xr = vx * dc / sim_res
+        yr = vy * dc / sim_res
+        zr = vz * dc / sim_res
+
+        # nearest neighbor interpolation
+        if interp == 'nearest':
+            # get nearest neighbors
+            xi = np.mod(np.around(xr).astype(int), sim_size[0])
+            yi = np.mod(np.around(yr).astype(int), sim_size[1])
+            zi = np.mod(np.around(zr).astype(int), sim_size[2])
+            out = np.asarray(cube[xi, yi, zi])
+
+        # tri-linear interpolation
+        # https://en.wikipedia.org/wiki/Trilinear_interpolation
+        elif interp == 'linear':
+            xd, yd, zd = xr % 1, yr % 1, zr % 1
+            x0 = np.mod(np.floor(xr).astype(int), sim_size[0])
+            x1 = np.mod(np.ceil(xr).astype(int), sim_size[0])
+            y0 = np.mod(np.floor(yr).astype(int), sim_size[1])
+            y1 = np.mod(np.ceil(yr).astype(int), sim_size[1])
+            z0 = np.mod(np.floor(zr).astype(int), sim_size[2])
+            z1 = np.mod(np.ceil(zr).astype(int), sim_size[2])
+            c00 = cube[x0, y0, z0] * (1 - xd) + cube[x1, y0, z0] * xd
+            c01 = cube[x0, y0, z1] * (1 - xd) + cube[x1, y0, z1] * xd
+            c10 = cube[x0, y1, z0] * (1 - xd) + cube[x1, y1, z0] * xd
+            c11 = cube[x0, y1, z1] * (1 - xd) + cube[x1, y1, z1] * xd
+            c0 = c00 * (1 - yd) + c10 * yd
+            c1 = c01 * (1 - yd) + c11 * yd
+            out = c0 * (1 - zd) + c1 * zd
+
+        else:
+            raise ValueError
 
     else:
-        zi = np.mod(np.around(dc / sim_res).astype(int), sim_size[2])
-        out = np.asarray(cube[..., zi])
+        # tile onto another cube
+        if interp =='nearest':
+            # nearest neighbor interpolation
+            zi = np.mod(np.around(dc / sim_res).astype(int), sim_size[2])
+            out = np.asarray(cube[..., zi])
+
+        elif interp == 'linear':
+            # linear interpolation
+            zr = dc / sim_res
+            zd = zr % 1
+            z0 = np.mod(np.floor(zr).astype(int), sim_size[2])
+            z1 = np.mod(np.ceil(zr).astype(int), sim_size[2])
+            out = cube[..., z0] * (1 - zd) + cube[..., z1] * zd
+
+        else:
+            raise ValueError
 
     return out
