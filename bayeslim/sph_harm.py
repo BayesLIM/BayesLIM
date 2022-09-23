1009b0a66e5f53c8b5ebe587a094db138db56e5d
Nicholas Kern <nkern@berkeley.edu>
2022-09-22 22:03:23 -0400

    modified:   bayeslim/sph_harm.py

--- bayeslim/sph_harm.py
+++ bayeslim/sph_harm.py
@@ -1270,8 +1270,7 @@ class AlmModel:
             # full transform
             return torch.einsum("...i,ij->...j", params, Ylm)
 
-    @staticmethod
-    def setup_angs(theta, phi):
+    def setup_angs(self, theta, phi):
         """
         If separate_variables, takes theta & phi grid points
         and meshes and flattens them. Otherwise return as is.
