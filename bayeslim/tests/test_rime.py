2d72edc979a5c9823b913724ab81da25501788ae
Nicholas Kern <nkern.astro@gmail.com>
2025-05-08 11:29:08 -0400

    modified:   test_rime.py

--- bayeslim/tests/test_rime.py
+++ bayeslim/tests/test_rime.py
@@ -13,20 +13,31 @@ from test_telescope import setup_Telescope, setup_Array
 freqs = torch.linspace(120e6, 130e6, 10)
 
 
-def setup_RIME():
-	telescope = setup_Telescope()
-	times = torch.linspace(2459861, 2459862, 10)
-	array = setup_Array(N=4, freqs=freqs)
-	sim_bls = array.get_bls(uniq_bls=True,  keep_autos=False)
-
+def setup_RIME(times, freqs, array, telescope):
 	beam = setup_PixBeam_Interp(freqs)
 	sky = setup_PixSky_Noise(freqs)
+	sim_bls = array.get_bls(uniq_bls=True,  keep_autos=False)
 
 	rime = ba.rime_model.RIME(
 		sky, telescope, beam, array, sim_bls, times, freqs
 	)
 
+	return rime
+
+
+def test_RIME():
+	times = torch.linspace(2459861, 2459862, 5)
+	telescope = setup_Telescope()
+	array = setup_Array(N=3, freqs=freqs)
+	sim_bls = array.get_bls(uniq_bls=True,  keep_autos=False)
+
+	rime = setup_RIME(times, freqs, array, telescope)
+
 	# test forward pass
 	with torch.no_grad():
 		vis = rime()
 	assert vis.data.shape == (1, 1, len(sim_bls), len(times), len(freqs))
+
+
+
+
